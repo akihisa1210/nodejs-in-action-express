cd4ced096b6dabf6dd532bb1ba1e94d9875128d6
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-08-11 14:01:54 +0900

    add logger

--- connect.js
+++ connect.js
@@ -1,4 +1,10 @@
 const connect = require('connect');
 
+const logger = (req, res, next) => {
+  console.log('%s %s', req.method, req.url);
+  next();
+};
+
 const app = connect();
+app.use(logger);
 app.listen(3000);
