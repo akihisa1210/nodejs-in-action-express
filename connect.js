0e5a171e2ec5c83f0b345edb7895afb4c069aba7
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-08-11 18:24:26 +0900

    add authentication feature

--- connect.js
+++ connect.js
@@ -1,6 +1,7 @@
 const connect = require('connect');
 
 const logger = (req, res, next) => {
+  // eslint-disable-next-line no-console
   console.log('%s %s', req.method, req.url);
   next();
 };
@@ -10,7 +11,34 @@ const hello = (req, res) => {
   res.end('hello world');
 };
 
+const isAuthenticatedUser = (user, pass) => {
+  // dummy authencication checker
+  if (user === 'hoge' && pass === 'fuga') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const restrict = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) return next(new Error('Unauthorizated'));
+
+  const parts = authorization.split(' ');
+  const scheme = parts[0];
+  const auth = Buffer.from(parts[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (scheme.search(/^basic$/i) && isAuthenticatedUser(user, pass)) {
+    return next();
+  } else {
+    return next(new Error('Unauthorizated'));
+  }
+};
+
 connect()
   .use(logger)
+  .use('/admin', restrict)
   .use(hello)
   .listen(3000);
