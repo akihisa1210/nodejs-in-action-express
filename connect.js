8d4d407127d52069baa272cd519a4e167f31676f
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-08-11 14:05:46 +0900

    add response

--- connect.js
+++ connect.js
@@ -5,6 +5,12 @@ const logger = (req, res, next) => {
   next();
 };
 
-const app = connect();
-app.use(logger);
-app.listen(3000);
+const hello = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
+};
+
+connect()
+  .use(logger)
+  .use(hello)
+  .listen(3000);
